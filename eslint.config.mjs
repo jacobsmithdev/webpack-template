e763e1968ee014624612ee0a4ce86119ce423247
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-08-08 09:48:22 -0600

    style: Format existing files with Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  globalIgnores([
-    "webpack.*.js",
-    "dist/",
-  ]),
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: { globals: globals.browser },
+    },
+    globalIgnores(['webpack.*.js', 'dist/']),
 ]);
